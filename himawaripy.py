1a5b6e65e21dbc8e75ee9010a2c2bf4bbc152c96
Baggykiin <baggykiin@jgeluk.net>
2016-02-04 21:01:05 +0100

    Remove remaining print() calls

--- himawaripy.py
+++ himawaripy.py
@@ -10,7 +10,6 @@ from urllib.request import urlopen
 
 from PIL import Image
 
-from utils import get_desktop_environment
 
 # Configuration
 # =============
@@ -27,17 +26,13 @@ def main():
     width = 550
     height = 550
 
-    print("Updating...")
     with urlopen("http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json") as latest_json:
         latest = strptime(loads(latest_json.read().decode("utf-8"))["date"], "%Y-%m-%d %H:%M:%S")
-
-    print("Latest version: {} GMT\n".format(strftime("%Y/%m/%d/%H:%M:%S", latest)))
 
     url_format = "http://himawari8.nict.go.jp/img/D531106/{}d/{}/{}_{}_{}.png"
 
     png = Image.new('RGB', (width*level, height*level))
 
-    print("Downloading tiles: 0/{} completed".format(level*level), end="\r")
     for x in range(level):
         for y in range(level):
             with urlopen(url_format.format(level, width, strftime("%Y/%m/%d/%H%M%S", latest), x, y)) as tile_w:
@@ -45,9 +40,6 @@ def main():
 
             tile = Image.open(BytesIO(tiledata))
             png.paste(tile, (width*x, height*y, width*(x+1), height*(y+1)))
-
-            print("Downloading tiles: {}/{} completed".format(x*level + y + 1, level*level), end="\r")
-    print("\nDownloaded\n")
 
     makedirs(split(output_file)[0], exist_ok=True)
     png.save(output_file, "PNG")
